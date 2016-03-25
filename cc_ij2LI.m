fd1149fa145e3babf13713d50b68e52afafb9993
BRIAN MICKEL <brianemickel@gmail.com>
2016-03-25 02:53:51 -0700

    ""

--- cc_ij2LI.m
+++ cc_ij2LI.m
@@ -2,6 +2,16 @@ function [linearindex_positions] = cc_ij2LI(initial_formation,marcher_i_position
 %convert i j subscripts to linear index
 
 linearindex_positions = sub2ind(size(initial_formation),marcher_i_positions,marcher_j_positions);
-
+%this works
+% tested with:
+    %initial_formation = zeros([4 5])
+    %marcher_i_positions = [1 1 2 3;1 2 3 4;1 1 1 1];
+    %marcher_j_positions = [3 3 3 3;2 2 2 2;1 2 3 4];
+% result
+    %linearindex_positions =
+    %9     9    10    11
+    %5     6     7     8
+    %1     5     9    13
 end
+
 
