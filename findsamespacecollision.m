55b6f9b4a8acc9c1e6735783bc4799cd2403d8a0
BRIAN MICKEL <brianemickel@gmail.com>
2016-03-26 21:31:06 -0700

    "closer to working"

--- findsamespacecollision.m
+++ findsamespacecollision.m
@@ -1,15 +1,25 @@
-function [C,ia,ic] = findsamespacecollision(linearindex_positions)
+function [] = findsamespacecollision(linearindex_positions)
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
+
+%COULD TRY TO FIX OR TRY DIFF WAY
+% for currentcolumn = 1:size(linearindex_positions,2)
+%     uniqueofcurrentcolumn = unique(linearindex_positions);
+%     countOfcurrentcolumn = hist(linearindex_positions,uniqueofcurrentcolumn);
+%     indexofRepeatedValue = (countOfcurrentcolumn~=1);
+%     repeatedValues = uniqueofcurrentcolumn(indexofRepeatedValue);
+%     numberOfAppearancesOfRepeatedValues = countOfcurrentcolumn(indexofRepeatedValue);
+% end
+
 for currentcolumn = 1:size(linearindex_positions,2)
-    
-    uniqueofcurrentcolumn = unique(linearindex_positions(:,currentcolumn));
-    countOfcurrentcolumn = hist(linearindex_positions(:,currentcolumn),uniqueofcurrentcolumn);
-    indexofRepeatedValue = (countOfcurrentcolumn~=1);
-    repeatedValues = uniqueofcurrentcolumn(indexofRepeatedValue);
-    numberOfAppearancesOfRepeatedValues = countOfcurrentcolumn(indexofRepeatedValue);
-    
+    % indices to unique values in column 3
+    [val, ind] = unique(linearindex_positions(:, currentcolumn), 'rows')
+    % duplicate indices
+    duplicate_ind = setdiff(1:size(linearindex_positions, 1), ind)
+    % duplicate values
+    duplicate_value = linearindex_positions(duplicate_ind, currentcolumn)
 end
+
 
 end
 
